ebeaecc97af49a40907d07051b7069b1ae370bba
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-13 07:33:30 +0100

    dashboard

--- nc-01-intro/src/app/page.js
+++ nc-01-intro/src/app/page.js
@@ -1,7 +1,8 @@
+//? App Folder içerisindeki page.js "/" route olan home sayfasıdır.
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello Next.js</h1>
+      <h1 className="text-3xl">Hello from Home Page !</h1>
     </main>
   );
 }
