a85e4dab688f1f9e3bdd010dc60ee837a5b61bec
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-13 07:46:35 +0100

    use client

--- nc-01-intro/src/app/page.js
+++ nc-01-intro/src/app/page.js
@@ -1,4 +1,7 @@
 //? App Folder içerisindeki page.js "/" route olan home sayfasıdır.
+
+//__ Next.js'de tüm componenter default olarak server componenttir. Client component kullanmak için "use client" direktifini kullanmamız gerekmektedir.
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
