04a8aab7f0d40bcfb321aaacbafcdd88a0ddc566
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-13 07:56:36 +0100

    routers

---
+++ nc-01-intro/src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-2xl text-center">About</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ nc-01-intro/src/app/calendar/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Calendar = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-2xl text-center">Dashboard</h1>
+    </div>
+  );
+};
+
+export default Calendar;
