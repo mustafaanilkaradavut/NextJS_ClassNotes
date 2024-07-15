3d1394ec410bab26fe256ab16ed581996ae6efb8
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-15 15:48:02 +0100

    json server

--- nc-01-intro/src/helpers/apiFunctions.jsx
+++ nc-01-intro/src/helpers/apiFunctions.jsx
@@ -6,6 +6,9 @@
 import next from "next";
 
 //*   const res = await fetch(URL, { next: { revalidate: 10 } }); belirlenen saniye cinsinden süre sonunda veriyi tekrar çek tekrar
+
+//!   json-server --watch db.json --port 8080
+//..  Bu kod ile next.js server kısmı açılır.
 const URL = `http://localhost:8080/users`;
 
 // //* force-cache
@@ -23,16 +26,29 @@ const URL = `http://localhost:8080/users`;
 //   return data;
 // }
 
-// //* revelidate
+//* revelidate
+// export async function getUsers() {
+//* Automatically revalidate data after a certain amount of time has passed. This is useful for data that changes infrequently and freshness is not as critical.
+
+//?   To revalidate data at a timed interval, you can use the next.revalidate option of fetch to set the cache lifetime of a resource (in seconds).
+
+//__ Burada diyoruz 20 saniye içerisinde kullanıcı ziyaret ederse güncel datayı 20 sn içinde update etmesini sağlar.
+//   const res = await fetch(URL, { next: { revalidate: 20 } });
+// 'force-cache' is the default, and can be omitted
+//  const res = await fetch(URL, { cache: 'force-cache' });
+
+//   if (!res.ok) {
+// This will activate the closest `error.js` Error Boundary
+//     throw new Error("Failed to fetch data");
+//   }
+//   const data = await res.json();
+//   return data;
+// }
+
+//* no-store
 export async function getUsers() {
-  //* Automatically revalidate data after a certain amount of time has passed. This is useful for data that changes infrequently and freshness is not as critical.
-
-  //?   To revalidate data at a timed interval, you can use the next.revalidate option of fetch to set the cache lifetime of a resource (in seconds).
-
-  //__ Burada diyoruz 20 saniye içerisinde kullanıcı ziyaret ederse güncel datayı 20 sn içinde update etmesini sağlar.
-  const res = await fetch(URL, { next: { revalidate: 20 } });
-  // 'force-cache' is the default, and can be omitted
-  //  const res = await fetch(URL, { cache: 'force-cache' });
+  //* This will fetch data dynamically, on every request.
+  const res = await fetch(URL, { cache: "no-store" });
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
