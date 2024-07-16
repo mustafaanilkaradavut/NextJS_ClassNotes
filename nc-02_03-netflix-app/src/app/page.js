a8298149dcb5f94a068f6b31962819e998e6fb9e
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-16 07:53:19 +0100

    register

--- nc-02_03-netflix-app/src/app/page.js
+++ nc-02_03-netflix-app/src/app/page.js
@@ -2,20 +2,21 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-center">
+    <main className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-center bg-cover">
       <div className="bg-black w-full h-full bg-opacity-50">
         <div className="text-white text-center relative top-2/4 m-auto">
-          <h1 className="text-5xl font-[900] mb-3">
-            Unlimited Movies, TV Shows and More
+          <h1 className="text-5xl font-[900] mb-3 ">
+            Unlimited movies, TV shows and more
           </h1>
-          <p className="text-2xl font-[400]">Watch Anywhere. Cancel Anytime</p>
+          <p className="text-2xl font-[400] ">
+            Watch anywhere. Cancel anytime.
+          </p>
           {/* //? next/link arka planda sayfayı önceden fetch edilir. Bu, client tarafı gezintilerin performansını iyileştirmek için kullanışlıdır. Görünüm alanındaki herhangi bir <Link />  önceden yüklenecektir. */}
           <Link href="/register">
-            <button className="btn-danger w-[250px]">Get Started</button>
+            <button className="btn-danger w-[250px] ">Get Started</button>
           </Link>
         </div>
       </div>
-      <h1>Netflix App</h1>
     </main>
   );
 }
