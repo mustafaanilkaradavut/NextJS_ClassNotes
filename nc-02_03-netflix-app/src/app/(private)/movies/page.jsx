04d27223929734f715842753f08775d62c32c832
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-16 15:04:20 +0100

    movies

--- nc-02_03-netflix-app/src/app/(private)/movies/page.jsx
+++ nc-02_03-netflix-app/src/app/(private)/movies/page.jsx
@@ -1,10 +1,16 @@
 import { getMovies } from "@/helpers/movieFunctions";
 import React from "react";
 import HeroSection from "./components/HeroSection";
+import MovieSection from "./components/MovieSection";
+
+export const metadata = {
+  title: "Movies",
+  description: "this is movies page  ",
+};
 
 const Movies = async () => {
   const movies = await getMovies("now_playing");
-  console.log(movies);
+  // console.log(movies);
   return (
     <div>
       <HeroSection
@@ -12,6 +18,12 @@ const Movies = async () => {
         title={movies[0]?.title}
         overview={movies[0]?.overview}
       />
+      <div className="px-4 md:px-12 mt-4">
+        <MovieSection title="NOW PLAYING" type="now_playing" />
+        <MovieSection title="POPULAR" type="popular" />
+        <MovieSection title="TOP RATED" type="top_rated" />
+        <MovieSection title="UPCOMING" type="upcoming" />
+      </div>
     </div>
   );
 };
